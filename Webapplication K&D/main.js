761fb602f2de108aa7c8ea1ded4b93f688cba83a
FMTamer <Frank.Mohammed.Tamer@gmail.com>
2019-10-23 00:53:25 +0200

    Completed plekken.html queries

--- Webapplication K&D/main.js
+++ Webapplication K&D/main.js
@@ -31,7 +31,7 @@ function mainCtrl($scope, $http){
 	$scope.Results_Arnhem = function(){
 
 		$scope.mySparqlEndpoint = 'http://192.168.2.5:7200/repositories/repo-VU' ;
-		$scope.mySparqlQuery = encodeURI('select * where {?sub ?pred ?obj}').replace(/#/, '%23');
+		$scope.mySparqlQuery = encodeURI('select * where {{?sub ?pred <http://www.semanticweb.org/rabie/ontologies/2019/9/groteoorlog/Arnhem>} UNION {<http://www.semanticweb.org/rabie/ontologies/2019/9/groteoorlog/Arnhem> ?pred ?obj}}').replace(/#/, '%23');
 
     $http( {
      	method: "GET",
@@ -39,7 +39,7 @@ function mainCtrl($scope, $http){
 			headers : {'Accept':'application/sparql-results+json', 'Content-Type':'application/sparql-results+json'}
     	} )
     	.success(function(data, status ) {
-			$scope.Results = data.results.bindings;
+			$scope.Results1 = data.results.bindings;
       })
     	.error(function(error ){
     	    console.log('Error '+error);
@@ -67,15 +67,14 @@ function mainCtrl($scope, $http){
 	$scope.Results_Nijmegen = function(){
 
 		$scope.mySparqlEndpoint = 'http://192.168.2.5:7200/repositories/repo-VU' ;
-		$scope.mySparqlQuery = encodeURI('select * where {?sub ?pred ?obj}').replace(/#/, '%23');
-
+		$scope.mySparqlQuery = encodeURI('select * where {{?sub ?pred <http://www.semanticweb.org/rabie/ontologies/2019/9/groteoorlog/Nijmegen>} UNION {<http://www.semanticweb.org/rabie/ontologies/2019/9/groteoorlog/Nijmegen> ?pred ?obj}}').replace(/#/, '%23');
     $http( {
      	method: "GET",
       url : $scope.mySparqlEndpoint + "?query=" + $scope.mySparqlQuery,
 			headers : {'Accept':'application/sparql-results+json', 'Content-Type':'application/sparql-results+json'}
     	} )
     	.success(function(data, status ) {
-			$scope.Results = data.results.bindings;
+			$scope.Results1 = data.results.bindings;
       })
     	.error(function(error ){
     	    console.log('Error '+error);
@@ -85,7 +84,7 @@ function mainCtrl($scope, $http){
 	$scope.Abstract_Nijmegen = function(){
 
 		$scope.mySparqlEndpoint = 'http://192.168.2.5:7200/repositories/repo-VU' ;
-		$scope.mySparqlQuery = encodeURI('select * where {?sub ?pred ?obj}').replace(/#/, '%23');
+		$scope.mySparqlQuery = encodeURI('select * where {SERVICE <http://dbpedia.org/sparql> { select ?obj where {<http://dbpedia.org/resource/Nijmegen> <http://dbpedia.org/ontology/abstract> ?obj. FILTER (langMatches(lang(?obj),"nl"))}}}').replace(/#/, '%23');
 
     $http( {
      	method: "GET",
@@ -103,7 +102,7 @@ function mainCtrl($scope, $http){
 	$scope.Results_Rotterdam = function(){
 
 		$scope.mySparqlEndpoint = 'http://192.168.2.5:7200/repositories/repo-VU' ;
-		$scope.mySparqlQuery = encodeURI('select * where {?sub ?pred ?obj}').replace(/#/, '%23');
+		$scope.mySparqlQuery = encodeURI('select * where {{?sub ?pred <http://www.semanticweb.org/rabie/ontologies/2019/9/groteoorlog/Rotterdam>} UNION {<http://www.semanticweb.org/rabie/ontologies/2019/9/groteoorlog/Rotterdam> ?pred ?obj}}').replace(/#/, '%23');
 
     $http( {
      	method: "GET",
@@ -111,7 +110,7 @@ function mainCtrl($scope, $http){
 			headers : {'Accept':'application/sparql-results+json', 'Content-Type':'application/sparql-results+json'}
     	} )
     	.success(function(data, status ) {
-			$scope.Results = data.results.bindings;
+			$scope.Results1 = data.results.bindings;
       })
     	.error(function(error ){
     	    console.log('Error '+error);
@@ -120,7 +119,7 @@ function mainCtrl($scope, $http){
 	$scope.Abstract_Rotterdam = function(){
 
 		$scope.mySparqlEndpoint = 'http://192.168.2.5:7200/repositories/repo-VU' ;
-		$scope.mySparqlQuery = encodeURI('select * where {?sub ?pred ?obj}').replace(/#/, '%23');
+		$scope.mySparqlQuery = encodeURI('select * where { SERVICE <http://dbpedia.org/sparql> { select ?objwhere {<http://dbpedia.org/resource/Rotterdam> <http://dbpedia.org/ontology/abstract> ?obj. FILTER (langMatches(lang(?obj),"nl"))}}}').replace(/#/, '%23');
 
     $http( {
      	method: "GET",
@@ -138,6 +137,24 @@ function mainCtrl($scope, $http){
 	$scope.Results_Wageningen = function(){
 
 		$scope.mySparqlEndpoint = 'http://192.168.2.5:7200/repositories/repo-VU' ;
+		$scope.mySparqlQuery = encodeURI('select * where {{?sub ?pred <http://www.semanticweb.org/rabie/ontologies/2019/9/groteoorlog/Wageningen>} UNION {<http://www.semanticweb.org/rabie/ontologies/2019/9/groteoorlog/Wageningen> ?pred ?obj}}').replace(/#/, '%23');
+
+    $http( {
+     	method: "GET",
+      url : $scope.mySparqlEndpoint + "?query=" + $scope.mySparqlQuery,
+			headers : {'Accept':'application/sparql-results+json', 'Content-Type':'application/sparql-results+json'}
+    	} )
+    	.success(function(data, status ) {
+			$scope.Results1 = data.results.bindings;
+      })
+    	.error(function(error ){
+    	    console.log('Error '+error);
+    	});
+	};
+
+	$scope.Abstract_Wageningen = function(){
+
+		$scope.mySparqlEndpoint = 'http://192.168.2.5:7200/repositories/repo-VU' ;
 		$scope.mySparqlQuery = encodeURI('select * where {?sub ?pred ?obj}').replace(/#/, '%23');
 
     $http( {
@@ -152,7 +169,79 @@ function mainCtrl($scope, $http){
     	    console.log('Error '+error);
     	});
 	};
-	$scope.Abstract_Wageningen = function(){
+
+	$scope.Results_Februaristaking = function(){
+
+		$scope.mySparqlEndpoint = 'http://192.168.2.5:7200/repositories/repo-VU' ;
+		$scope.mySparqlQuery = encodeURI('select * where {?sub ?pred ?obj}').replace(/#/, '%23');
+
+    $http( {
+     	method: "GET",
+      url : $scope.mySparqlEndpoint + "?query=" + $scope.mySparqlQuery,
+			headers : {'Accept':'application/sparql-results+json', 'Content-Type':'application/sparql-results+json'}
+    	} )
+    	.success(function(data, status ) {
+			$scope.Results = data.results.bindings;
+      })
+    	.error(function(error ){
+    	    console.log('Error '+error);
+    	});
+	};
+
+	$scope.Results_Capitulatie = function(){
+
+		$scope.mySparqlEndpoint = 'http://192.168.2.5:7200/repositories/repo-VU' ;
+		$scope.mySparqlQuery = encodeURI('select * where {?sub ?pred ?obj}').replace(/#/, '%23');
+
+    $http( {
+     	method: "GET",
+      url : $scope.mySparqlEndpoint + "?query=" + $scope.mySparqlQuery,
+			headers : {'Accept':'application/sparql-results+json', 'Content-Type':'application/sparql-results+json'}
+    	} )
+    	.success(function(data, status ) {
+			$scope.Results = data.results.bindings;
+      })
+    	.error(function(error ){
+    	    console.log('Error '+error);
+    	});
+	};
+
+	$scope.Results_Slag_Arnhem = function(){
+
+		$scope.mySparqlEndpoint = 'http://192.168.2.5:7200/repositories/repo-VU' ;
+		$scope.mySparqlQuery = encodeURI('select * where {?sub ?pred ?obj}').replace(/#/, '%23');
+
+    $http( {
+     	method: "GET",
+      url : $scope.mySparqlEndpoint + "?query=" + $scope.mySparqlQuery,
+			headers : {'Accept':'application/sparql-results+json', 'Content-Type':'application/sparql-results+json'}
+    	} )
+    	.success(function(data, status ) {
+			$scope.Results = data.results.bindings;
+      })
+    	.error(function(error ){
+    	    console.log('Error '+error);
+    	});
+	};
+
+	$scope.Results_Slag_Nijmegen = function(){
+
+		$scope.mySparqlEndpoint = 'http://192.168.2.5:7200/repositories/repo-VU' ;
+		$scope.mySparqlQuery = encodeURI('select * where {?sub ?pred ?obj}').replace(/#/, '%23');
+
+    $http( {
+     	method: "GET",
+      url : $scope.mySparqlEndpoint + "?query=" + $scope.mySparqlQuery,
+			headers : {'Accept':'application/sparql-results+json', 'Content-Type':'application/sparql-results+json'}
+    	} )
+    	.success(function(data, status ) {
+			$scope.Results = data.results.bindings;
+      })
+    	.error(function(error ){
+    	    console.log('Error '+error);
+    	});
+	};
+	$scope.Results_Bom_Rotterdam = function(){
 
 		$scope.mySparqlEndpoint = 'http://192.168.2.5:7200/repositories/repo-VU' ;
 		$scope.mySparqlQuery = encodeURI('select * where {?sub ?pred ?obj}').replace(/#/, '%23');
